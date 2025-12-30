0351920376b72d6a8cdac0f1b80d31c1f31c8d28
authenticationrizqdaan-maker <authenticationrizqdaan@gmail.com>
2025-12-30 08:19:50 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   },
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    assetsDir: 'assets'
   }
 });
